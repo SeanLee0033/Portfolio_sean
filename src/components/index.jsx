90b0c75c8d371ba7e2e19ca3d446043f39426a9d
SeanLee0033 <Sean.nov@gmail.com>
2022-11-14 02:14:13 -0700

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 export { default as Navigation } from './navigation';
 export { default as Home } from './home';
-export { default as About } from './about';
+export { default as Skills } from './about';
 export { default as Work } from './work/work';
 export { default as Testimonials } from './testimonials';
 export { default as Contact } from './contact';
