08d0595974a4aa064d7be584996508b0ec7b69be
SeanLee0033 <Seanlee.nov@gmail.com>
2022-11-14 02:15:17 -0700

    Update testimonials.jsx

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -27,7 +27,7 @@ const Testimonials = () => {
             <div className="testimonial">
               <div className="testimonial__speech-bubble">
                 <p>
-                  "Sean worked as a website developer intern at JSolution for 3 months. His work is always finished on time, and he always goes the extra mile to ensure the client requirements are met. He is a highly-skilled, willing to learn new things, and helpful team member with a positive attitude. Working with Sean has been a great experience for everyone at our company due to his team work."
+                  "Sean worked as a website developer intern at JSolution for 3 months. His work is always finished on time, and he always goes the extra mile to ensure the client requirements are met. He is a highly-skilled, willing to learn new things, and helpful team member with a positive attitude. Working with Sean has been a great experience for everyone at our company due to his team."
                 </p>
                 <p className="name">
                   <a href="https://www.linkedin.com/in/jae-chung-56026384/">
